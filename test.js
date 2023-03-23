bfb81235a0bcfaf2a54ac23888cfa608ede58459
toqeer-hussain <63150363+toqeer-hussain@users.noreply.github.com>
2023-03-23 13:29:37 +0500

    new workflow

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ describe("User Endpoints", () => {
   it("GET /user should show all users", async () => {
     const res = await requestWithSupertest.get("/");
     expect(res.status).toEqual(200);
-    process.exit();
+    process.exit(0);
   });
 });
